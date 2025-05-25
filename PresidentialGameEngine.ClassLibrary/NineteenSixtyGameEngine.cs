1590480534bf944290625cd896c2ebdc85ca3631
johnhibbert <johnhibbert@users.noreply.github.com>
2025-05-25 17:48:37 -0400

    Moved to own class and namespace.

--- PresidentialGameEngine.ClassLibrary/NineteenSixtyGameEngine.cs
+++ PresidentialGameEngine.ClassLibrary/NineteenSixtyGameEngine.cs
@@ -1,44 +1,10 @@
-﻿using PresidentialGameEngine.ClassLibrary.Data;
+﻿using PresidentialGameEngine.ClassLibrary.Abstracts;
+using PresidentialGameEngine.ClassLibrary.Data;
 using PresidentialGameEngine.ClassLibrary.Enums;
 using PresidentialGameEngine.ClassLibrary.Interfaces;
 
 namespace PresidentialGameEngine.ClassLibrary
 {
-
-    public abstract class GameOfMomentum<PlayersEnum> where PlayersEnum : Enum
-    {
-        public Dictionary<PlayersEnum, int> PlayerMomentum { get; init; }
-
-        protected GameOfMomentum()
-        {
-            PlayerMomentum = [];
-
-            foreach (PlayersEnum player in ((PlayersEnum[])Enum.GetValues(typeof(PlayersEnum))))
-            {
-                PlayerMomentum.Add(player, 0);
-            }
-        }
-
-        public int GetPlayerMomentum(PlayersEnum player)
-        {
-            return PlayerMomentum[player];
-        }
-
-        public void GainMomentum(PlayersEnum player, int amount)
-        {
-            PlayerMomentum[player] += amount;
-        }
-
-        public void LoseMomentum(PlayersEnum player, int amount)
-        {
-            PlayerMomentum[player] -= amount;
-            if (PlayerMomentum[player] < 0)
-            {
-                PlayerMomentum[player] = 0;
-            }
-        }
-
-    }
 
     public class GenericPresidentialGameEngine : GameOfMomentum<Player>
     {
