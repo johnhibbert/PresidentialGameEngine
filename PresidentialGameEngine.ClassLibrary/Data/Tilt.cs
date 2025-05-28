62669407a0a2af7439c9dea635133d1afb0ca5b9
johnhibbert <johnhibbert@users.noreply.github.com>
2025-05-28 12:44:37 -0400

    Initial implementation of Tilt

---
+++ PresidentialGameEngine.ClassLibrary/Data/Tilt.cs
@@ -0,0 +1,8 @@
+﻿namespace PresidentialGameEngine.ClassLibrary.Data
+{
+    public class Tilt<PlayersEnum>
+    {
+        public required PlayersEnum Player { get; set; }
+        public int StartingSupport { get; set; }
+    }
+}
