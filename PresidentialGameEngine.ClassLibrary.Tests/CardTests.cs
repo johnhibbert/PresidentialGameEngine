95c9ffc12b879fcbc863f86575ea46f6dcac44fb
johnhibbert <johnhibbert@users.noreply.github.com>
2025-05-29 05:12:45 -0400

    Added card test for compiler.

---
+++ PresidentialGameEngine.ClassLibrary.Tests/CardTests.cs
@@ -0,0 +1,30 @@
+﻿using PresidentialGameEngine.ClassLibrary.Data;
+
+namespace PresidentialGameEngine.ClassLibrary.Tests
+{
+    [TestClass]
+    public class CardTests
+    {
+        [TestMethod]
+        public void ToString_ReturnsExpectedInformation()
+        {
+            var Card = new Card() 
+            {
+                Index = 1,
+                Title = "Test",
+                Text = "Test",
+                Event = (engine, player, choices) =>
+                {
+                    //Nothing
+                },
+                AreChangesValid = (choices) =>
+                {
+                    return true;
+                },
+            };
+
+            Assert.IsTrue(Card.ToString().Contains(Card.Index.ToString()));
+            Assert.IsTrue(Card.ToString().Contains(Card.Title.ToString()));
+        }
+    }
+}
