9ec549639b1115c6b7a38eb9e0750db75bca4e05
johnhibbert <johnhibbert@users.noreply.github.com>
2025-06-01 07:18:52 -0400

    Created new changes class

--- PresidentialGameEngine.ClassLibrary/Data/PlayerChosenChanges.cs
+++ PresidentialGameEngine.ClassLibrary/Data/PlayerChosenChanges.cs
@@ -56,6 +56,100 @@ namespace PresidentialGameEngine.ClassLibrary.Data
             return returnValue;
         }
     }
+
+
+    //We could change this to be just Game State Changes or something, unsure if that's more clear
+    public class NewMasterPlayerChosenChanges<PlayersEnum, IssuesEnum, StatesEnum, RegionsEnum>
+     where PlayersEnum : Enum
+     where IssuesEnum : Enum
+     where StatesEnum : Enum
+     where RegionsEnum : Enum
+    {
+
+        //This is going to be a mess.
+        public List<SupportChange<PlayersEnum, IssuesEnum>> IssueChanges { get; internal set; }
+        public List<SupportChange<PlayersEnum, StatesEnum>> StateChanges { get; internal set; }
+        public List<SupportChange<PlayersEnum, RegionsEnum>> EndorsementChanges { get; internal set; }
+        public List<SupportChange<PlayersEnum, RegionsEnum>> MediaSupportChanges { get; internal set; }
+
+
+
+        public NewMasterPlayerChosenChanges()
+        {
+            IssueChanges = [];
+            StateChanges = [];
+            EndorsementChanges = [];
+            MediaSupportChanges = [];
+        }
+
+        public int TotalIssueChanges
+        {
+            get { return IssueChanges.Select(x => x.Change).Sum(); }
+
+        }
+        public int HighestIssueChange
+        {
+            get { return IssueChanges.Max(x => x.Change); }
+        }
+
+        public int TotalStateChanges
+        {
+            get { return StateChanges.Select(x => x.Change).Sum(); }
+
+        }
+        public int HighestStateChange
+        {
+            get { return StateChanges.Max(x => x.Change); }
+        }
+
+        public int TotalEndorsementChanges
+        {
+            get { return EndorsementChanges.Select(x => x.Change).Sum(); }
+
+        }
+        public int HighestEndorsementChange
+        {
+            get { return EndorsementChanges.Max(x => x.Change); }
+        }
+
+        public int TotalMediaChanges
+        {
+            get { return MediaSupportChanges.Select(x => x.Change).Sum(); }
+
+        }
+        public int HighestMediaChange
+        {
+            get { return MediaSupportChanges.Max(x => x.Change); }
+        }
+
+        //The existing game does not appear to ever require
+        //More than one type of change from players.
+        public bool ContainsExactlyOneTypeOfChange() 
+        {
+            var counter = 0;
+
+            if(TotalIssueChanges > 0) 
+            {
+                counter++;
+            }
+            if (TotalStateChanges > 0)
+            {
+                counter++;
+            }
+            if (TotalEndorsementChanges > 0)
+            {
+                counter++;
+            }
+            if (TotalMediaChanges >0)
+            {
+                counter++;
+            }
+
+            return counter == 1;
+        }
+    }
+
+
 }
 
 
