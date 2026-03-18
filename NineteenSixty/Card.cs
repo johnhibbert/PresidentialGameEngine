df87a358a2776d5dca3e96ca503644cc016ebbfe
johnhibbert <john.andrew.hibbert@gmail.com>
2026-03-18 18:17:20 -0400

    Additional files.

---
+++ NineteenSixty/Card.cs
@@ -0,0 +1,40 @@
+using PresidentialGameEngine.ClassLibrary.Data;
+using PresidentialGameEngine.ClassLibrary.Engines;
+using PresidentialGameEngine.ClassLibrary.Enums;
+using PresidentialGameEngine.ClassLibrary.Interfaces;
+
+namespace NineteenSixty;
+
+public class Card : ITypicalCard<Player,Issue,State,Region,Affiliation,EventType>, ICard
+{
+    //<TPlayer, TIssue, TState, TRegion, TAffiliation, TEventType>     
+    public int Index { get; init; }
+    public required string Text { get; init; }
+    public required string Title { get; init; }
+
+    public int CampaignPoints { get; init; }
+    public int RestCubes => 4 - CampaignPoints;
+    public EventType EventType { get; init; }
+    public bool RequiresPlayerInput { get; init; }
+    public required Predicate<PlayerChosenChanges<Player, Issue, State, Region>> AreChangesValid { get; init; }
+    public required Action<NineteenSixtyGameEngine, Player, PlayerChosenChanges<Player, Issue, State, Region>> Event
+    {
+        get;
+        init;
+    }
+
+    public Affiliation Affiliation { get; init; }
+    public State State { get; init; }
+    public Issue Issue { get; init; }
+    
+    public override string ToString()
+    {
+        return $"{Title} [{Index}]";
+    }
+
+    public string ToLongString()
+    {
+        return $"{ToString()}: {Text}";
+    }
+};
+    
---
+++ NineteenSixty/IEngine.cs
@@ -0,0 +1,20 @@
+using PresidentialGameEngine.ClassLibrary.Data;
+using PresidentialGameEngine.ClassLibrary.Engines;
+
+namespace NineteenSixty;
+
+public interface IEngine
+    /*
+    <TPlayer, TLeader, TIssue, TState, TRegion>
+    where TPlayer : Enum
+    where TLeader : Enum
+    where TIssue : Enum
+    where TState : Enum
+    where TRegion : Enum
+    */
+{
+    //GameState GetGameState();
+
+    //public void ImplementChanges(SetOfChanges changes);
+
+}
