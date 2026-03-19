799733876912802f102f5a5c0245adbd6ae153b9
johnhibbert <john.andrew.hibbert@gmail.com>
2026-03-19 08:24:23 -0400

    More manifest-level changes.

---
+++ NineteenSixty.Tests/Fixtures/EngineFixtures.cs
@@ -0,0 +1,56 @@
+using PresidentialGameEngine.ClassLibrary.Components;
+using NineteenSixty.Enums;
+
+namespace NineteenSixty.Tests.Fixtures;
+
+
+public class EngineFixtures
+{
+    private static Engine GetGameEngine() 
+    {
+        TestStubsFakesAndMocks.SeededRandomnessProviderForTesting seed = new();
+        
+        /*
+        ComponentCollection<Player, Leader, Issue, State, Region, Card> compColl = new()
+        {
+            MomentumComponent = new AccumulatingComponent<Player>(),
+            IssueSupportComponent = new SupportComponent<Player, Leader, Issue>(),
+            StateSupportComponent = new CarriableSupportComponent<Player, Leader, State>(),
+            IssuePositioningComponent = new PositioningComponent<Issue>(),
+            PoliticalCapitalComponent = new PoliticalCapitalComponent<Player>(seed, 12),
+            PlayerLocationComponent = new PlayerLocationComponent<Player, State>(NineteenSixty.PlayerStartingPositions),
+            RestComponent = new AccumulatingComponent<Player>(),
+            EndorsementComponent = new SupportComponent<Player, Leader, Region>(),
+            MediaSupportComponent = new SupportComponent<Player, Leader, Region>(),
+            ExhaustionComponent = new ExhaustionComponent<Player>(),
+            CardComponent = new CardComponent<Player, Card>(seed, NineteenSixty.GMTCards),
+            StaticDataComponent = new StaticDataComponent<State, Player, Region>(NineteenSixty.StateData)
+        };
+
+        return new NineteenSixtyGameEngine(compColl);
+        */
+
+        var momentumComponent = new AccumulatingComponent<Player>();
+        var issueSupportComponent = new SupportComponent<Player, Leader, Issue>();
+        var stateSupportComponent = new CarriableSupportComponent<Player, Leader, State>();
+        var issuePositioningComponent = new PositioningComponent<Issue>();
+        var politicalCapitalComponent = new PoliticalCapitalComponent<Player>(seed, 12);
+        var playerLocationComponent = new PlayerLocationComponent<Player, State>(Manifest.PlayerStartingPositions);
+        var restComponent = new AccumulatingComponent<Player>();
+        var endorsementComponent = new SupportComponent<Player, Leader, Region>();
+        var mediaSupportComponent = new SupportComponent<Player, Leader, Region>();
+        var exhaustionComponent = new ExhaustionComponent<Player>();
+        //var cardComponent = new CardComponent<Player, Card>(seed, Manifest.GMTCards);
+        //var staticDataComponent = new StaticDataComponent<State, Player, Region>(Manifest.StateData);
+
+        //        public StaticDataComponent(IDictionary<TState, ILocationData<TState, TPlayer, TRegion>> locationData)
+        // {
+        
+        //Dictionary<State, StateData>
+        
+        throw new NotImplementedException();
+        
+        //return new Engine
+
+    }
+}
--- NineteenSixty/Data/StateData.cs
+++ NineteenSixty/Data/StateData.cs
@@ -1,10 +1,24 @@
-using NineteenSixty.Enums;
-using PresidentialGameEngine.ClassLibrary.Data;
-using PresidentialGameEngine.ClassLibrary.Interfaces;
-
-namespace NineteenSixty.Data;
-
-public class StateData(State state, Region region, int electoralVotes, Player tilt, int startingSupport)
-    : LocationData<State, Player, Region>(state, region, electoralVotes, tilt, startingSupport)
-{
-};
+// using NineteenSixty.Enums;
+// using PresidentialGameEngine.ClassLibrary.Data;
+// using PresidentialGameEngine.ClassLibrary.Interfaces;
+//
+// namespace NineteenSixty.Data;
+//
+// public class StateData(State state, Region region, int electoralVotes, Player tilt, int startingSupport)
+//     : ILocationData<State, Player, Region>
+// {
+//     public State State { get; init; } = state;
+//
+//     public int ElectoralVotes { get; init; } = electoralVotes;
+//
+//     public Player Tilt { get; init; } = tilt;
+//
+//     public Region Region { get; init; } = region;
+//
+//     public int StartingSupport { get; init; } = startingSupport;
+//
+//     public override string ToString()
+//     {
+//         return $"{State} [{ElectoralVotes}]";
+//     }
+// };
